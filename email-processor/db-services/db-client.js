1e22f388e1efd5628937fdd1acd3901574ca0397
Gregory Garcia <gregory.garcia@authoritypartners.com>
2022-09-25 09:02:09 -0400

    Adding gmail provider, starting db config.

---
+++ email-processor/db-services/db-client.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const CONFIG_PATH = path.join(process.cwd(), '..', 'configuration-files', 'db-config.json');
+
+const { MongoClient } = require('mongodb');
+
+async function getDb() {
+    try {
+        const content = await fs.readFile(CONFIG_PATH);
+        const config = JSON.parse(content);
+        return new MongoClient(config.connectionString).db(config.dbName);
+    } catch (err) {
+        return null;
+    }
+}
+
+const client = loadClient();
+
+client.connect();
+
+export default {
+    getConfigData: async function () {
+        let db = await getDb();
+        if (db) {
+            return null;
+        }
+        return null;
+    }
+};
---
+++ email-processor/index.js
@@ -0,0 +1,3 @@
+import dbClient from './db-services/db-client';
+
+const configData = dbClient
